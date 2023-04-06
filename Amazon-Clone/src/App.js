b3d65175c609a8c814079260955e59b4c7ee7a1e
Rinku <32036922+Rinku3@users.noreply.github.com>
2023-04-06 13:43:11 +0530

    Add files via upload

---
+++ Amazon-Clone/src/App.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import Header from './Header';
+import Home from './Home'
+import { BrowserRouter as Router, Switch, Route}
+from "react-router-dom";
+import {useEffect} from "react";
+import Checkout from  "./Checkout";
+import Login from "./Login";
+import { auth } from './firebase';
+import {useStateValue} from './StateProvider';
+function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser=>{
+      console.log('The User is >>>', authUser);
+      if(authUser){
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+  return (
+    <Router>
+      <div className="App"> 
+        <Switch>
+          <Route path="/login">
+           <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header/>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>  
+    
+  );
+}
+
+export default App;
